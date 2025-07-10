0dfd9b914e46078678412eeecc9a6eccc91ad421
stelleron <shreyasdonti223@gmail.com>
2025-07-10 16:49:01 +0530

    Printing ASCII art if no arguments given

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Receive arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Handle additional arguments
+    // == 0 ARGUMENTS
+    if args.len() == 1 {
+        println!(r#"  ____              _   "#);
+        println!(r#" |  _ \            | |  "#);
+        println!(r#" | |_) | ___   ___ | |_ "#);
+        println!(r#" |  _ < / _ \ / _ \| __|"#);
+        println!(r#" | |_) | (_) | (_) | |_ "#);
+        println!(r#" |____/ \___/ \___/ \__|"#);
+    }
+    // == 1 ARGUMENT
+    // == 2+ ARGUMENTS
 }
