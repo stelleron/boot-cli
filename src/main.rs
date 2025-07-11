448da5709a491674551d20f6264f308f7d78b227
stelleron <shreyasdonti223@gmail.com>
2025-07-11 12:58:18 +0530

    Added 'Hello World' from Ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-mod term_print;
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
 
-use std::env;
-use term_print::ansi;
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
 
-fn main() {
-    // Receive arguments
-    let args: Vec<String> = env::args().collect();
-
-    // Handle additional arguments
-    // == 0 ARGUMENTS
-    if args.len() == 1 {
-        println!(r"  ____                    __      ");
-        println!(r" /\  _`\                 /\ \__   ");
-        println!(r" \ \ \L\ \    ___     ___\ \ ,_\  ");
-        println!(r"  \ \  _ <'  / __`\  / __`\ \ \/  ");
-        println!(r"   \ \ \L\ \/\ \L\ \/\ \L\ \ \ \_ ");
-        println!(r"    \ \____/\ \____/\ \____/\ \__\");
-        println!(r"     \/___/  \/___/  \/___/  \/__/");
-        println!("{}Boot -- Your Personal Project Manager CLI Tool {}", ansi::BOLD, ansi::RESET);
-        println!("Run {} boot help {} to get info on commands", ansi::ITALIC, ansi::RESET);
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
     }
-    // == 1 ARGUMENT
-    // == 2+ ARGUMENTS
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
